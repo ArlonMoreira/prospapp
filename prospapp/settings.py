ba5988aa7b050a84558532fa28a6c072cc889c3f
ArlonMoreira <arlonsilva28@gmail.com>
2024-06-25 23:44:50 -0300

    ajuste CORS ALLOWED_HOSTS

--- prospapp/settings.py
+++ prospapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-np27$th(n#ko2n0w-@8++xcoju3tkl)^0=wh&2mbs-kki@w_!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
