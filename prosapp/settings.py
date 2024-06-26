4327a9df36bf29014118484411c47eb138591a49
ArlonMoreira <arlonsilva28@gmail.com>
2024-06-25 23:15:23 -0300

    deploy on render

--- prosapp/settings.py
+++ prosapp/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'assets'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
