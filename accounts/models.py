fb61b959d003b28f8085f21550256cc388da7f22
ArlonMoreira <arlonsilva28@gmail.com>
2024-06-27 23:05:44 -0300

    create app accounts and custom Users

---
+++ accounts/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
+
+class UsersManager(BaseUserManager):
+
+    # No método create_user, **extra_fields é usado para aceitar campos adicionais que possam ser fornecidos ao criar um usuário. 
+    # Isso torna a função mais flexível, permitindo que você passe quaisquer campos extras que o modelo de usuário possa ter, 
+    # sem precisar modificá-la toda vez que adicionar um novo campo ao modelo.
+    def create_user(self, email, password=None, **extra_fields):
+        
+        if not email:
+            raise ValueError('E-mail é um campo obrigatório.')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        user = self.create_user(
+            email=email,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+
+        return user
+
+class Users(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField( #E-mail será o campo principal para autenticação do usuário.
+        verbose_name="E-mail",
+        unique=True,
+        blank=False,
+        null=False
+    )
+    full_name = models.CharField(
+        max_length=125,
+        verbose_name="Nome completo",
+        unique=False,
+        blank=False,
+        null=False        
+    )
+    doc_number = models.IntegerField(
+        verbose_name="CPF",
+        unique=True,
+        blank=False,
+        null=False
+    )
+    is_active = models.BooleanField(
+        verbose_name="Ativo",
+        default=True
+    )
+    is_admin = models.BooleanField(
+        verbose_name="Administrador",
+        default=False
+    )
+    is_superuser = models.BooleanField(
+        verbose_name="Superusuário",
+        default=False
+    )
+    date_joined = models.DateTimeField(
+        verbose_name="Data/Cadastro",
+        default=timezone.now
+    )
+
+    USERNAME_FIELD = 'email' #No django USERNAME_FIELD é utilizado para identificar qual campo será utilizado como identificador único, nesse caso, USERNAME_FIELD
+    REQUIRED_FIELDS = []  #Determinar quais campos são obrigatórios para criar um super usuário.
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
--- prospapp/settings.py
+++ prospapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configurações do usuário.
+
+AUTH_USER_MODEL = 'accounts.Users'
