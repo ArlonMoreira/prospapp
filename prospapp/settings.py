ecd1c26486b96c0886880bd90e94e930091608ba
ArlonMoreira <arlonsilva28@gmail.com>
2024-07-13 16:51:16 -0300

    ALLOW HOST prosperefisio.com.br

--- prospapp/settings.py
+++ prospapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY_PROSPAPP']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prospapp.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['prospapp.onrender.com', 'localhost', 'prosperefisio.com.br']
 
 
 # Application definition
