5b99796fe624f7d190f38b5a0a9c427d98b35b5f
Arlon Moreira <arlonsilva28@gmail.com>
2025-04-29 22:28:29 -0300

    cadastrar locais de ponto

--- point/api/url.py
+++ point/api/url.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .views import LocalViews
+from .views import LocalViews, GetLocalViews
 
 urlpatterns = [
-    re_path('register/', LocalViews.as_view())
+    re_path('register/', LocalViews.as_view()),
+    re_path('list/(?P<companyId>\d+)/$', GetLocalViews.as_view())
 ]
--- point/api/views.py
+++ point/api/views.py
@@ -1,14 +1,31 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import LocalSerialier
+from .serializers import LocalSerialier, Local
 from company.models import CompanyPeople
+
+class GetLocalViews(generics.GenericAPIView):
+    serializer_class = LocalSerialier
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, companyId=None):
+        companyPeople = CompanyPeople.objects.filter(user=request.user)
+
+        if not companyPeople.exists():
+            return Response({'message': 'Esse usuário não está vinculado a nenhum companhia.', 'data': []}, status=status.HTTP_400_BAD_REQUEST)
+        
+        locals = Local.objects.filter(company_id=companyId)
+
+        locals = self.serializer_class(locals, many=True).data
+
+        return Response({'message': 'Locais de ponto recuperado', 'data': locals}, status=status.HTTP_200_OK)     
+
 
 class LocalViews(generics.GenericAPIView):
     serializer_class = LocalSerialier
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         companyPeople = CompanyPeople.objects.filter(user=request.user)
 
         if not companyPeople.exists():
