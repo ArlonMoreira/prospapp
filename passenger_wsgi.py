1ccd52dae577035565495807ea6641a80fe296a8
Arlon Moreira <arlonsilva28@gmail.com>
2025-07-17 11:48:28 -0300

    config

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,11 @@
 import sys, os
 
-# Define o diretório raiz do projeto
-sys.path.insert(0, os.path.dirname(__file__))
+# Caminho da raiz do projeto Django
+sys.path.insert(0, "/home/prospere/prospapp")
 
-# Nome do seu módulo de configurações (verifique se é exatamente isso)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'prospapp.settings'
+# Nome do módulo de configurações do Django
+os.environ["DJANGO_SETTINGS_MODULE"] = "prospapp.settings"
 
+# Importa a aplicação WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
