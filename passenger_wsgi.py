f487f32a2ad42fb7b362d9f65ad503f4d0e7e79a
Arlon Moreira <arlonsilva28@gmail.com>
2025-07-17 11:26:04 -0300

    htaccess

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,9 @@
 import sys, os
+
+# Define o diretório raiz do projeto
 sys.path.insert(0, os.path.dirname(__file__))
 
+# Nome do seu módulo de configurações (verifique se é exatamente isso)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'prospapp.settings'
 
 from django.core.wsgi import get_wsgi_application
